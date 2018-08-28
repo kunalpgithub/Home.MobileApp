755a275acba842e7dba93451ea0214118e49bcaa
kunalpgithub <kunal.ahmedabad@gmail.com>
2018-08-28 16:31:47 +0530

    Implemented EF migration.

---
+++ Home.Infrastructure/HomeContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home.Infrastructure
+{
+    public class HomeContext: DbContext
+    {
+        public HomeContext(DbContextOptions<HomeContext> options):base(options) {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+    public class Employee:AuditInfo {
+        public int EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+    }
+
+    public class AuditInfo {
+        public int CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
--- Home.WebAPI/Startup.cs
+++ Home.WebAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Home.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -26,6 +28,9 @@ namespace Home.WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=HomeApp;Trusted_Connection=True;ConnectRetryCount=0";
+            //services.AddDbContext<HomeContext>(options => options.UseSqlServer(Configuration.GetConnectionString("HomeAppDataBase")));
+            services.AddDbContext<HomeContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
