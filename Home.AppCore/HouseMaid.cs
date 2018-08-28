afc1efd2c316acabc628c7eeafd825a81e034551
kunalpgithub <kunal.ahmedabad@gmail.com>
2018-08-28 15:24:30 +0530

    added infrastructure and appCore projects. Moved model from xamarin project to AppCore.

---
+++ Home.AppCore/HouseMaid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home.AppCore
+{
+    class HouseMaid:BaseWorker
+    {
+        public MaidWork JobType { get; set; }
+    }
+
+    public enum MaidWork {
+        Cleaning,
+        Cooking
+    }
+}
---
+++ Home.AppCore/IWorker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Home.AppCore
+{
+    public interface IWorker
+    {
+         string FirstName { get; set; }
+         string LastName { get; set; }
+         string JobTitle { get; set; }
+         DateTime StartDate { get; set; }
+         DateTime EndDate { get; set; }
+         void DidWork(bool didWork);
+    }
+}
